a0878203ee1cba4fc763b3fea9f2d7eb48d15a2e
Jaja-owiny <jasonowiny20@gmail.com>
2022-12-14 12:07:50 -0800

    4-puts program correction

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
-#include <stdio.h>
-/**
- * main -entry point 
- *
- * Return - 0 after printing the function
- */
-int main(void)
+ #include <stdio.h>
+ /**
+  * main -entry point 
+  *
+  * Return - 0 after printing the function
+  */
+ int main(void)
  {
-   puts("Programming is like a building a multilingual puzzle \n");
-   return (0);	  
+ puts("\"Programming is like a building a multilingual puzzle \n");
+ return (0);	  
  } 
