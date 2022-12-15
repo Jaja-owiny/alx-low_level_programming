30e29a89ad0780980884676c407c9802ef18a254
Jaja-owiny <jasonowiny20@gmail.com>
2022-12-15 08:32:03 -0800

    puts script update

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,12 @@
- #include <stdio.h>
- /**
-  * main -entry point 
-  *
-  * Return - 0 after printing the function
-  */
- int main(void)
- {
- puts("\"Programming is like a building a multilingual puzzle \n");
- return (0);	  
- } 
+#include <stdio.h>
+
+/**
+ * main - Prints Programming is like a building a multilingual puzzle
+ * Return: Return 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like a building a multilingual puzzle \n");
+	return (0);
+}
