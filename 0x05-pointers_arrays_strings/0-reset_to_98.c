b071943cfd40948c2350cbd80b879646fec8b64b
Jaja-owiny <jasonowiny20@gmail.com>
2022-12-19 09:51:17 -0800

    Int value modification

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ *reset_to_98 - modifies the value of an int
+ *@n: value to replace the signed integer
+ *Return: Return nothing
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
