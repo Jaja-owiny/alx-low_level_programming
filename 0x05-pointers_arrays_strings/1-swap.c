aab346f348d12a8b40abb0efa0dc828814dbb061
Jaja-owiny <jasonowiny20@gmail.com>
2022-12-19 11:47:54 -0800

    Int value manipulation

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *swap_int - swaps the integers
+ *@a: first int
+ *@b: secnd int
+ *Return: nothing
+ */
+
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	int d = *b;
+
+	*a = c;
+	*b = d;
+}
